15c81e03be990e078010e45e28979e9869f75503
Zmagis <eglegustaite@gmail.com>
2020-09-03 17:20:00 +0300

    fix fetch function

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ function App() {
       .then((data) => {
         console.log(data);
         console.log(data.text);
+        const json = JSON.parse(data.data.text).photos.photo;
+        const newA = [...data, ...json];
+        setData([...removedDuplicates(newA)]);
+        setLoading(false);
       });
   }, [page]);
 
